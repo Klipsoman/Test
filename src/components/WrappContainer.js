1ddb7f7e4739e891c89e5fb9f454a1b748d8b0e0
Andrey <Sneik92@yandex.ru>
2021-05-29 15:12:22 +0300

    some change

--- src/components/WrappContainer.js
+++ src/components/WrappContainer.js
@@ -9,6 +9,27 @@ class MainContainer extends React.Component {
     glassSymbol: "BTCUSDT",
   };
 
+  // setNewGlassSymbol = (symbol) => {
+  //   this.setState({
+  //     glassSymbol: symbol,
+  //   });
+  //   axios
+  //     .get(`api/v3/depth?symbol=${symbol}&limit=500`)
+  //     .then((res) => res.data)
+  //     .then((res) => this.props.getGlassAC(res.bids, res.asks))
+  //     .catch((err) => console.log(err));
+  // };
+
+  // componentDidMount() {
+  //   setInterval(() => {
+  //     axios
+  //     .get(`api/v3/depth?symbol=${this.state.glassSymbol}&limit=500`)
+  //     .then((res) => res.data)
+  //     .then((res) => this.props.getGlassAC(res.bids, res.asks))
+  //     .catch((err) => console.log(err));
+  //   }, 3000);
+  // }
+
   setNewGlassSymbol = (symbol) => {
     this.setState({
       glassSymbol: symbol,
@@ -20,16 +41,23 @@ class MainContainer extends React.Component {
       .catch((err) => console.log(err));
   };
 
-  componentDidMount() {
-    setInterval(() => {
-      axios
-      .get(`api/v3/depth?symbol=${this.state.glassSymbol}&limit=500`)
-      .then((res) => res.data)
-      .then((res) => this.props.getGlassAC(res.bids, res.asks))
-      .catch((err) => console.log(err));
-    }, 3000);
-   
+    componentDidMount() {
+
+      const getGlassSymbol = async () => {
+        try{
+        const res = await axios.get(`api/v3/depth?symbol=${this.state.glassSymbol}&limit=500`)
+        const data = await res.data
+        this.props.getGlassAC(data.bids, data.asks)
+      } catch(err) {
+        console.log('Warning is: ' + err)
+      } 
   }
+  setInterval(() => {
+    getGlassSymbol()
+  }, 3000);
+  
+}
+
 
   render() {
     return <Wrapp {...this.props} setNewGlassSymbol={this.setNewGlassSymbol} glassSymbol={this.state.glassSymbol}/>;
