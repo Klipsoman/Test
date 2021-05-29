1fbe24e11edbfaf046b79a54f74750d95a7fd1a3
Andrey <Sneik92@yandex.ru>
2021-05-29 16:27:34 +0300

    add some media

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './Table.module.css'
 
 
 const Table = ({glass:{bidsAmount,bidsPrise,bidsTotal,asksAmount,asksPrise,asksTotal}}) => {
+  let [screenWidth] = useState(window.innerWidth)
+if(screenWidth < 768) {
+  return (
+    <div className={style.tableWrapper}>
+            <table className={style.table}>
+              <thead>
+                <tr>
+                  <th>Amount</th>
+                  <th className={style.rightCenter}>Price</th>          
+                  <th>Amount</th>
+                  <th>Price</th>             
+                </tr>
+              </thead>
+              <tbody>
+                {bidsAmount?.map((bid, i)=>{
+                  return <tr key={i}>
+                    <td className={style.bidsColor}>{bid}</td>
+                    <td className={style.rightCenter + ' ' + style.bidsColor}>{bidsPrise[i]}</td>       
+                    <td className={style.leftCenter + ' ' + style.asksColor}>{asksAmount[i]}</td>
+                    <td className={style.asksColor}>{asksPrise[i]}</td>                
+                  </tr>
+                })}
+              </tbody>    
+            </table>
+            </div>
+  )
+}
   
 return (
 <div className={style.tableWrapper}>
